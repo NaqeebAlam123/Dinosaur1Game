d565cd1a9e4ba9fc2cc859fb2243f5dc1b3932b4
NaqeebAlam <mala0036@student.monash.edu>
2021-04-29 21:58:01 +0500

    Added Allosaur Class

--- src/game/Player.java
+++ src/game/Player.java
@@ -1,11 +1,6 @@
 package game;
 
-import edu.monash.fit2099.engine.Action;
-import edu.monash.fit2099.engine.Actions;
-import edu.monash.fit2099.engine.Actor;
-import edu.monash.fit2099.engine.Display;
-import edu.monash.fit2099.engine.GameMap;
-import edu.monash.fit2099.engine.Menu;
+import edu.monash.fit2099.engine.*;
 
 /**
  * Class representing the Player.
@@ -13,6 +8,7 @@ import edu.monash.fit2099.engine.Menu;
 public class Player extends Actor {
 
 	private Menu menu = new Menu();
+	private int ecoPoints=0;
 
 	/**
 	 * Constructor.
@@ -25,9 +21,37 @@ public class Player extends Actor {
 		super(name, displayChar, hitPoints);
 	}
 
+	public   void incrementPoints(int points){
+		ecoPoints=ecoPoints+points;
+	}
+	public void deductPoints(int points){ecoPoints=ecoPoints-points;}
+
+	public int getEcoPoints() {
+		return ecoPoints;
+	}
+
 	@Override
 	public Action playTurn(Actions actions, Action lastAction, GameMap map, Display display) {
 		// Handle multi-turn Actions
+		Location thisLocation=map.locationOf(this);
+		/*for (Exit exit : thisLocation.getExits()) {
+			Location destination = exit.getDestination();*/
+
+		if (thisLocation.getGround() instanceof Tree){
+			Tree tree=(Tree) thisLocation.getGround();
+
+
+				for (Fruit fruits:tree.getFruits()) {
+					if (!(fruits.hasCapability(FruitStatus.DROPPED)) && fruits.getPortable()) {
+						actions.add(new HarvestingFruits(fruits));
+					}
+				}
+			if (actions.size() == 0) {
+				System.out.println("Player searched the tree but couldnot found riped ones");
+
+			}
+			}
+
 		if (lastAction.getNextAction() != null)
 			return lastAction.getNextAction();
 		return menu.showMenu(this, actions, display);
