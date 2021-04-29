7d73e82411aa16cd2d17fae7e5aca5a5efd38b59
zche0138 <zche0138@student.monash.edu>
2021-04-29 23:44:49 +1000

    Upload Corpse Class

---
+++ src/game/Corpse.java
@@ -0,0 +1,17 @@
+package src.game;
+import edu.monash.fit2099.engine.Item;
+
+public abstract class Corpse extends Item{
+
+    public Corpse(String name, char displayCharacter, boolean b){
+        super(name,displayCharacter,false);
+    }
+
+    abstract void setCorpseDuration(int duration);
+
+    abstract int getCorpseDuration();
+
+    abstract boolean checkDecayed();
+
+    abstract boolean decreaseDuration();
+}
