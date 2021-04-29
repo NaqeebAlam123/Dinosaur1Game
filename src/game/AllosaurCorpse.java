4c5ad9b55eee81c936fe67636529b0809d762ac7
zche0138 <zche0138@student.monash.edu>
2021-04-29 23:45:32 +1000

    child class of Corpse, Allosaur Corpse added

---
+++ src/game/AllosaurCorpse.java
@@ -0,0 +1,43 @@
+package src.game;
+
+public class AllosaurCorpse extends Corpse{
+    private int duration = 20;
+    public AllosaurCorpse(String name, char displayChar){
+        super(name,displayChar,false);
+    }
+
+    public void setCorpseDuration(int duration ){
+        this.duration = duration;
+    }
+
+    /**
+     *  method to get the remaining duration
+     * @return remaining duration of corpse before decay
+     */
+    public int getCorpseDuration(){
+        return duration;
+    }
+
+    /**
+     * A method that constantly checks and determine decayed corpse
+     * @return the status of the corpse whether it is decayed(true/false)
+     */
+    public boolean checkDecayed(){
+        boolean decayed = false;
+        if(duration <= 0){
+            decayed = true;
+        }
+        return decayed;
+    }
+
+
+    public boolean decreaseDuration(){
+        boolean valid = false;
+        if (duration > 0){
+            duration -= 1;
+            valid = true;
+        }
+        return valid;
+    }
+
+}
